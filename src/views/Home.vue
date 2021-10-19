354988d7e7387d22022528dcfd92f7d7b64302ed
Cristobal Fernandez <cristobal.fernandez@usach.cl>
2021-10-19 15:34:55 -0300

    Footer Home
    Se me le olvidó

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,24 +8,7 @@
     <v-content>
       <router-view/>
     </v-content>
-
-      <v-footer
-      color="#FFEFD2"
-      v-bind="localAttrs"
-      :padless="padless"
-    >
-      <v-card
-        color="#FFEFD2"
-        flat
-        tile
-        width="100%"
-        class="text-center"
-      >
-        <v-card-text class="black--text">
-          <strong>Devance</strong> — <strong>Grupo 2</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+    <Footer></Footer>
     
   </v-app>
 </template>
@@ -33,6 +16,7 @@
 <script>
 
 import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 export default {
   name: 'Home',
@@ -41,6 +25,7 @@ export default {
   }),
   components: {
     Navbar,
+    Footer,
   }
 };
 </script>
