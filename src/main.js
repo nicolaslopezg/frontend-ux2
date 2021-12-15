d683d46fc88c675f556de29e8db0ce2c1c08ee2a
nicolaslopezg <nicolas.lopez.g@usach.cl>
2021-12-15 17:07:28 -0300

    Feat: Conexion Firebase

--- src/main.js
+++ src/main.js
@@ -2,11 +2,42 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 Vue.config.productionTip = false
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgWJ76oJj2szfeLD2CL_RsQkrLl6emKs8",
+  authDomain: "frontend-ux.firebaseapp.com",
+  projectId: "frontend-ux",
+  storageBucket: "frontend-ux.appspot.com",
+  messagingSenderId: "892906669711",
+  appId: "1:892906669711:web:aa8df991eca8d4e232d2fa",
+  measurementId: "G-715Y19MXNT"
+};
+
+// Initialize Firebase
+let firebaseApp;
+
+const apps = getApps();
+if (!apps.length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = apps[0];
+}
+const db = getFirestore(firebaseApp, {});
 
 new Vue({
   vuetify,
   router,
   render: h => h(App)
 }).$mount('#app')
+
+export{db};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,13 @@
     <v-content>
       <router-view/>
     </v-content>
+
+    <v-btn class="ma-10" @click="loadUser" >Hola</v-btn>
+    <v-btn class="ma-10" @click="setUser" >Guardar</v-btn>
+    {{texto}}
+    <v-btn class="ma-10" @click="leerDatos" >Leer</v-btn>
+
+
     <Footer></Footer>
     
   </v-app>
@@ -17,15 +24,45 @@
 
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import { doc, getDoc, setDoc, onSnapshot } from "firebase/firestore";
+import{db} from "../main.js";
 
 export default {
   name: 'Home',
   data: () => ({
+    texto: "",
     drawer: false,
   }),
   components: {
     Navbar,
     Footer,
+  },
+  methods:{
+    async loadUser() {
+    const userRef = doc(db, "users/1");
+    try {
+      const loadedUser = await getDoc(userRef);
+      if (loadedUser !== null) {
+        const {
+          name,
+        } = loadedUser.data();
+        console.log(name);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    },
+  async setUser(){
+    await setDoc(doc(db, "users","US"), {
+    name: "Los Angeles",
+  });
+    },
+  async leerDatos(){
+    onSnapshot(doc(db, "users", "LA"), (doc) => {
+      this.texto=doc.data();
+    console.log("Current data: ", doc.data());
+    });
+  }
   }
 };
 </script>
